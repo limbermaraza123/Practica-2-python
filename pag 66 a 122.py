f37bef91be2746d83e68386b5698757b45cc7d90
limbermaraza123 <33768991+limbermaraza123@users.noreply.github.com>
2017-12-03 21:26:06 -0400

    Create pag 66 a 122.py

---
+++ pag 66 a 122.py
@@ -0,0 +1,327 @@
+PAG 66
+
+a=3.14159*radio*radio
+return a
+
+
+
+PAG 67
+
+int r1,r2
+as=area(r1)-area(r1)
+print as
+
+
+
+PAG 69
+
+suma = 0
+for i in range(1,(n/2):
+	if residuo(n,i)==0:
+		suma=suma+i
+	if suma==n:
+		return true
+	else
+		return false
+    
+    
+    
+    
+PAG 70
+
+for i in range(1,101):
+	if es_perfeto
+	print n
+  
+  
+  
+  PAG 71
+  
+  if residuo(n,4)==0&&residuo(n,100)!=0||residuo(n,400)==0:
+
+		else
+		return true
+		return false
+		
+    
+    
+    
+    PAG 72
+    
+    for i in range(1,2000)
+      if es_perfecto(n):
+        print n
+        
+        
+     
+    PAG 74
+    
+    if c== Destructor:
+	print Destructor
+	elif c==Fragata
+		print Fragata
+	elif c==Misilera:
+		print Misilera
+	elif c==Buque:
+		print Buque
+  else 
+  print "no existe",
+  
+  
+  
+  PAG 76
+  
+ producto =1
+ for i in range(1,n)
+	print i
+  
+  
+ PAG 77
+ 
+ n
+f=factorial(n)
+print f
+
+
+
+PAG 79
+
+if n==0||n==1:
+	return n
+else
+	for i in range(2,n)
+		y=y+x
+		x=y-x
+	return y
+		
+    
+    
+    
+ PAG 81
+ 
+ y=1
+while k>0
+	if residuo(k,2)==1:
+		y=y*a
+	a=a*a
+	b=[b/2]
+return y
+
+
+
+PAG 82
+
+result = 0
+while numero>0:
+	digito=residuo(numero,10)
+	result=result*10+digito
+	numero=numero/10
+return result
+
+
+
+PAG 84
+
+x1=1
+repeat
+	x=x1
+	x1=((2*x+a/x*x)/3)
+until|x-x1|<="ERROR"
+return x1
+
+
+
+
+PAG 85
+
+def abs
+n=abs(b)
+producto=1
+for i in range(1,n):
+	producto=producto*a
+if b<0:
+	producto=1/producto
+return producto
+
+
+
+PAG 87
+
+x=n1*2
+x=x+5
+y=x*50
+y=y+n2
+z=y+365
+z=z-615
+return z
+
+
+
+PAG 89
+
+import math
+
+rho=sqrt(a*a+b*b)
+ang=arcsen(b/rho)
+rhon=rhon^n
+angn=n*ang
+an=rhon*cos(angn)
+bn=rhon*sen(angn)
+print an,bn
+
+
+
+
+PAG 91
+
+h=(b-a)/n
+suma=0
+for i in range(1,n-1)
+	x=a+i*h
+	suma=suma+f(x)
+t=0.5*h*(f(a)+f(b)+2*suma)
+return t
+
+
+
+
+PAG 94
+
+for i in range(1,n)
+	a[i]
+  
+  
+  
+  
+  
+  PAG 95
+  
+  n
+Vleer(a,n)
+Vleer(b,n)
+p=Vproducto(a,b,n)
+print p,
+
+
+
+
+
+PAG 97
+
+suma=0
+for i in range(1,n):
+	suma=suma+a[i]*b[i]
+return suma
+
+
+
+PAG 98
+
+suma=0
+for i in range(1,n):
+	suma=suma+x[i]
+prom=suma/n
+return prom
+
+
+
+
+PAG 100
+
+suma=0
+prom=media(x,n)
+for i in range(1,n):
+	suma=suma+(x[i]-prom)^2
+desv= sqrt(suma/n)
+return desv
+
+
+
+
+PAG 101
+
+n
+Vleer(x,n)
+print media(x,n)
+print dst(x,n)
+
+
+
+
+PAG 103
+
+for i in range(1,n):
+	for j in range(1,n):
+		if i==j:
+			a[i][j]=1
+		else
+			a[i][j]=0
+      
+      
+      
+      
+ PAG 104
+ 
+ for i in range(1,n):
+	for j in range(1,n):
+			a[i][j]
+		
+    
+    
+ PAG 106
+ 
+ for i in range(1,n):
+	for j in range(1,m):
+		c[i][j]=0
+		for k in range(1,l)
+			c[i][j]=c[i][j]+a[i][k]*b[k][j]
+			
+      
+      
+      
+      
+ PAG 110
+ 
+ b[n]=a[n]
+for i in range(-1,0):
+	b[i]=a[i]+B*b[i+1]
+return b[0]
+
+
+
+
+  PAG 113
+  
+  i=1
+c=xF
+while c>0&&i<=n:
+	c=B*c
+	b[i]=c
+	c=c-b[i]
+	i=i+1
+return i+1
+
+
+
+
+  PAG 116
+  
+  for i in rage(1,n):
+	r=0
+	for j in rage(1,i-1):
+		r=r*10+d[j]
+		d[j]=r/2
+		r=residuo(r,2)
+	d[i]=5
+  
+  
+  
+   PAG  118
+   
+   m=n/2
+if residuo(n,2)==1:
+	M=x[m+1]
+else
+	M=(x[m]+x[m+1])/2
+return M
+  
